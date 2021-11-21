ca6a0451cde67f4a8fed3220122fa49386be0b5b
ushu3323 <redspower10@gmail.com>
2021-11-21 19:26:48 -0300

    script 'sync': modelos y registro admin

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   db: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '3306',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'delilah_resto'
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || "3306",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "delilah_resto",
+  },
+  cache: {
+    host: process.env.CACHE_HOST || "localhost",
+    port: process.env.CACHE_PORT || "6379",
+    user: process.env.CACHE_USER || "",
+    password: process.env.CACHE_PASSWORD || "",
   },
   server: {
     port: process.env.NODE_PORT || 3000,
-    key: process.env.KEY || "YouShouldntReadThis",
-  }
-}
+    key: process.env.KEY || "YouShouldntReadThis:O",
+  },
+  admin: {
+    username: process.env.ADMIN_USERNAME || "admin",
+    password: process.env.ADMIN_PASSWORD || "admin",
+  },
+};
