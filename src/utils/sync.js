11918072760196cd9bf1a2bc3f6149aa26ec8d3e
ushu3323 <redspower10@gmail.com>
2021-11-21 19:26:48 -0300

    script 'sync': modelos y registro admin

--- src/utils/sync.js
+++ src/utils/sync.js
@@ -1,20 +1,36 @@
-const User = require('../user/model/User');
-const Product = require('../product/model/Product');
-const Order = require('../order/model/Order');
-const OrderProducts = require('../order/model/OrderProducts');
-const paymentMethod = require('../paymentMethod/model/PaymentMethod');
+const User = require("../user/model/User");
+const Product = require("../product/model/Product");
+const Order = require("../order/model/Order");
+const OrderProducts = require("../order/model/OrderProducts");
+const paymentMethod = require("../paymentMethod/model/PaymentMethod");
 
-const sequelize = require('../connection/sequelize');
+const config = require("../config");
+const sequelize = require("../connection/sequelize");
+const { sha256 } = require("js-sha256");
 
 const sync_models = async () => {
   try {
-    await sequelize.authenticate()
-    console.log("Authenticated successfully")
-    await sequelize.sync({ force: true, alter:false });
+    await sequelize.authenticate();
+    console.log("Authenticated successfully");
+    await sequelize.sync({ force: true, alter: false });
     console.log(`Syncronized "${sequelize.config.database}" database!`);
   } catch (error) {
     console.error(error.cause);
   }
+
+  // Register admin user
+  await User.findOrCreate({
+    where: { id: 1, isAdmin: true },
+    order: [["id", "ASC"]],
+    defaults: {
+      username: config.admin.username,
+      password: config.admin.password,
+      fullName: "superuser",
+      email: "testuser@example",
+      isAdmin: true,
+    },
+  });
+  process.exit();
 
   /* const [testUser, user_created] = await User.findOrCreate({
     where: { email: 'testuser@example.com' },
@@ -39,6 +55,7 @@ const sync_models = async () => {
     }
   });
 
+  // Get the first order of 'testUser'
   let testOrder = (await testUser.getOrders({
     where: { status: 'new' },
     limit: 1,
@@ -60,7 +77,11 @@ const sync_models = async () => {
   });
   console.log('user', finalUser);
   */
-}; 
+};
 
-
-sync_models()
+param = process.argv[2];
+if (param == "-s") {
+  sync_models();
+} else {
+  console.log(`Unknown parameter: ${param}`);
+}
