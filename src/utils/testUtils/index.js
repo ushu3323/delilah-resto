3f045cd21682bfd59d1eeb03d795e3857ab38f78
ushu3323 <redspower10@gmail.com>
2021-11-22 14:51:25 -0300

    rutina de metodo de pago (CRUD)

--- src/utils/testUtils/index.js
+++ src/utils/testUtils/index.js
@@ -1,6 +1,8 @@
 const users = require("./users");
 const products = require("./products");
 const orders = require("./orders");
+const paymentMethods = require("./paymentMethods");
+
 
 const bulkInit = async (utils) => {
   for (util of utils) {
@@ -19,5 +21,6 @@ module.exports = {
   bulkDelete,
   users,
   products,
-  orders
+  orders,
+  paymentMethods
 }
--- src/utils/testUtils/paymentMethods.js
+++ src/utils/testUtils/paymentMethods.js
@@ -1,12 +1,36 @@
-async function initPlaceholders() {
+const paymentMethodRepository = require('../../paymentMethod/repository/paymentMethod.repository');
 
+const placeholders = {
+  creditCard: {
+    name: 'Tarjeta de credito',
+    enabled: false,
+  },
+  debitCard: {
+    name: 'Tarjeta de debito',
+    enabled: false,
+  },
+  cash: {
+    name: 'Efectivo',
+    enabled: false,
+  },
 }
 
-async function deletePlaceholders() {
+const initPlaceholders = async () => {
+  for (let key in placeholders) {
+    if(key === "creditCard") continue;
+    const payMethod = await paymentMethodRepository.create(placeholders[key]);
+    placeholders[key].id = payMethod.id;
+  }
+}
 
+const deletePlaceholders = async () => {
+  for (let key in placeholders) {
+    await paymentMethodRepository.del.byId(placeholders[key].id);
+  }
 }
 
 module.exports = {
   initPlaceholders,
-  deletePlaceholders
+  deletePlaceholders,
+  placeholders
 }
