88e783d7690647c6ec2a9ccd3deed1e90e022322
ushu3323 <redspower10@gmail.com>
2022-03-30 01:01:12 -0300

    passport: deserialize/serialize user session

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -1,13 +1,26 @@
 const passport = require('passport');
+const userRepository = require('../user/repositories/user.repository');
 
 passport.serializeUser(function (user, done) {
-  console.log("serializing user: ", user);
-  done(null, user);
+  console.log(`serializing user (strategy:${user.passportStrategy}): ${user.username} #${user.id}`);
+  const finalUser = {
+    id: user.id,
+    email: user.email,
+    strategy: user.passportStrategy,
+  }
+  done(null, finalUser);
 });
 
-passport.deserializeUser(function (user, done) {
-  console.log("deserializing user: ", user);
-  done(null, user);
+passport.deserializeUser(async (serializedUser, done) => {
+  console.log("deserializing token: ", serializedUser.email);
+  try {
+    const user = await userRepository.get.byId(serializedUser.id);
+    if (!user) return done(null, false);
+
+    return done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 
