7605653a5a1584989bc895924712a4fb0ac816e0
ushu3323 <redspower10@gmail.com>
2022-04-04 23:24:51 -0300

    use error info messages from LocalStrategy

--- src/auth/routes/auth.routes.js
+++ src/auth/routes/auth.routes.js
@@ -11,10 +11,15 @@ route.get('/logout', (req, res, next) => {
 });
 
 // Login a user
-route.post('/local/login', passport.authenticate('local'), (req, res) => {
-  const user = req.user.toJSON(); // user is a sequelize object
-  delete user.password;
-  res.json({ msg: "autenticado correctamente", user, error: false });
+route.post('/local/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err);
+    if (info) return res.status(401).json({ msg: info.message, error: true });
+    let jsonUser = user.toJSON(); // user is a sequelize object
+    delete jsonUser.password;
+    req.login(user, next);
+    res.json({ msg: "autenticado correctamente", user: jsonUser, error: false });
+  })(req, res, next);
 });
 
 module.exports = route;
