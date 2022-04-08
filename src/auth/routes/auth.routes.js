f08c2d765d06d77f950a75b622d656893161dbfe
ushu3323 <redspower10@gmail.com>
2022-04-08 14:01:02 -0300

    add google-strategy routes (login and callback)

--- src/auth/routes/auth.routes.js
+++ src/auth/routes/auth.routes.js
@@ -1,5 +1,6 @@
-const route = require('express').Router();
 const passport = require('passport');
+const route = require('express').Router();
+
 
 route.get('/logout', (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -17,9 +18,26 @@ route.post('/local/login', (req, res, next) => {
     if (info) return res.status(401).json({ msg: info.message, error: true });
     let jsonUser = user.toJSON(); // user is a sequelize object
     delete jsonUser.password;
-    req.login(user, next);
+    req.login(user, (err) => err ? next(err) : null);
     res.json({ msg: "autenticado correctamente", user: jsonUser, error: false });
   })(req, res, next);
 });
+
+route.get('/google/login', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+}));
+
+route.get(
+  '/google/callback',
+  passport.authenticate('google', {authInfo: true}),
+  (req, res, next) => {
+    if (req.info && !req.user) return res.json({ msg: req.info.message, error: true })
+
+    const user = req.user.toJSON();
+    delete user.credentials
+    req.login(req.user, (err) => err ? next(err) : null);
+    res.json({ msg: "test", user });
+  }
+);
 
 module.exports = route;
