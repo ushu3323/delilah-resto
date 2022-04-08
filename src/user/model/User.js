a5c5816389fa97783a0d3890492453935d9a2ddd
ushu3323 <redspower10@gmail.com>
2022-04-08 01:01:15 -0300

    clean User model script

--- src/user/model/User.js
+++ src/user/model/User.js
@@ -40,23 +40,6 @@ User.init({
 }, {sequelize, modelName: "user"});
 
 User.Order = User.hasMany(Order);
- // helps to show the user asociated to the order
 Order.belongsTo(User, {as: 'user', foreignKey: 'userId'});
-
-/* (async () => {
-    await sequelize.sync({force: false});
-
-    await User.create({
-        username:"admin",
-        fullName:"ADMIN",
-        email: "admin@example.com",
-        phoneNumber: "123-456",
-        address: "Street Address",
-        password: "secretpass",
-        isAdmin: true,
-        enabled: true,
-    })
-})() */
-
 
 module.exports = User;
